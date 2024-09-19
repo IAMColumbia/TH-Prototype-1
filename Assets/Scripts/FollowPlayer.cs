6b5fe19911849daa0fcb58ef387eff2789639ed0
Fulgore2002 <typryorhitchcock1@gmail.com>
2024-09-18 22:35:29 -0500

    Made the offset into a Vector3 variable
    Wouldn't work unless I added "UnityEngine." to it

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private UnityEngine.Vector3 offset = new UnityEngine.Vector3(0, 5, -7);
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,6 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         //Offset the camera behind the behind by adding to the player's position
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
     }
 }
